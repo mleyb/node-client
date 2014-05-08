acd5c3e17f8e2e2c370284190e88f04bb1af82ec
Mark Leybourne <markleybourne@Marks-MacBook-Pro.local>
2014-05-08 20:15:01 +0100

    Basic GET

---
+++ get.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+var options = {
+  	host: 'www.google.com',
+  	port: 80,
+  	path: '/'
+};
+
+http.get(options, function(resp) {
+
+	console.log('Got response: ' + resp.statusCode);
+
+	var buffer = "";
+
+   	resp.on("data", function(data) { 
+   		buffer = buffer + data; 
+   	});
+
+   	resp.on("end", function(data) { 
+   		console.log(buffer); 
+   	});
+}).on("error", function(e) {
+	console.log("Got error: " + e.message);
+});
